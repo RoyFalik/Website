a7e1268f3f7e9138e8cbc1727466d82485010166
Roy Falik <roy.falik@utexas.edu>
2015-10-06 03:09:04 -0500

    added Nivo slider files

---
+++ controller.js
@@ -0,0 +1,19 @@
+$(window).load(function() {
+	console.log("Swag");
+    $('#slider').nivoSlider({
+        effect:'fade', // Specify sets like: 'fold,fade,sliceDown'
+        animSpeed:500, // Slide transition speed
+        pauseTime:3000, // How long each slide will show
+        startSlide:0, // Set starting Slide (0 index)
+        directionNav:true, // Next & Prev navigation
+        directionNavHide:true, // Only show on hover
+        controlNav:false, // 1,2,3... navigation
+        controlNavThumbs:false, // Use thumbnails for Control Nav
+        keyboardNav:false, // Use left & right arrows
+        pauseOnHover:true, // Stop animation while hovering
+        manualAdvance:false, // Force manual transitions
+        captionOpacity:0.8, // Universal caption opacity
+        prevText: 'Prev', // Prev directionNav text
+        nextText: 'Next' // Next directionNav text
+     });
+});
